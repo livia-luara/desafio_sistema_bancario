d8d15fd84da6da5e210c01bd3252974e238288f2
Livia Luara <147873544+livia-luara@users.noreply.github.com>
2024-09-20 10:57:26 -0300

    Nova versão sistema_bancario.py
    Adicionado horário em extrato e limite de transações diárias

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,3 +1,9 @@
+from datetime import timedelta, datetime
+import pytz
+
+data_atual = datetime.now(pytz.timezone("America/Sao_Paulo"))
+mascara_ptbr = "%d/%m/%y %H:%M"
+
 menu = """
 
 =============
@@ -13,6 +19,8 @@ saldo = 0
 limite = 500
 extrato = ""
 numero_saques = 0
+numero_depositos = 0
+limite_transacoes = 10
 limite_saques = 3
 
 while True:
@@ -22,9 +30,15 @@ while True:
     if opcao == "1":
         valor = float(input("Informe a seguir o valor desejado do depósito:  "))
 
+        excede_transacoes = numero_saques + numero_depositos >= limite_transacoes
+
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
+            numero_depositos += 1
+
+        elif excede_transacoes:
+            print("Operação falhou! Número máximo de transações diárias excedido. Tente novamente mais tarde.")
 
         else:
             print("Operação falhou! O valor informado é inválido. Repita.")
@@ -33,6 +47,8 @@ while True:
         valor = float(input("Informe o valor desejado do saque:  "))
 
         excede_saldo = valor > saldo
+
+        excede_transacoes = numero_saques + numero_depositos >= limite_transacoes
 
         excede_limite = valor > limite
 
@@ -43,6 +59,9 @@ while True:
 
         elif excede_limite:
             print("Operação falhou! O valor do saque excede o seu limite.")
+
+        elif excede_transacoes:
+            print("Operação falhou! Número máximo de transações diárias excedido. Tente novamente mais tarde.")
 
         elif excede_saques:
             print("Operação falhou! Número máximo de saques diários excedido. Tente novamente mais tarde.")
@@ -57,6 +76,7 @@ while True:
 
     elif opcao == "3":
         print("\n================ EXTRATO BANCÁRIO ================")
+        print(data_atual.strftime(mascara_ptbr))
         print("Não houve transações bancárias." if not extrato else extrato)
 
         print(f"\nSaldo: R$ {saldo:.2f}")
